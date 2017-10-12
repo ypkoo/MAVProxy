de9c7b5b1a648b5783ea8d4b1ca352767a409580
ypkoo <ypkoo93@gmail.com>
2017-10-12 20:00:43 +0900

    packetmonitor full functioning

--- MAVProxy/modules/mavproxy_packetmonitor.py
+++ MAVProxy/modules/mavproxy_packetmonitor.py
@@ -18,89 +18,136 @@ import sys
 from pymavlink import mavutil
 import errno
 import time
+from datetime import datetime
 
 from MAVProxy.modules.lib import mp_module
 from MAVProxy.modules.lib import mp_util
 from MAVProxy.modules.lib import mp_settings
 
 class MAVPacketLogger(object):
-	def __init__(self):
-		
+	def __init__(self, filename):
+		self.f = open(filename, 'w')
+
+	def log(self, m):
+		self.f.write("[%s] %s\n" % (datetime.now().strftime('%H:%M:%S.%f'), m))
+
+	def __del__(self):
+		self.f.close()
+
+
 
 
 class PacketMonitorModule(mp_module.MPModule):
-    def __init__(self, mpstate):
-        """Initialise module"""
-        super(PacketMonitorModule, self).__init__(mpstate, "packetmonitor", "monitoring MAVLink packets")
-        self.status_callcount = 0
-        self.boredom_interval = 10 # seconds
-        self.last_bored = time.time()
+	def __init__(self, mpstate):
+		"""Initialise module"""
+		super(PacketMonitorModule, self).__init__(mpstate, "packetmonitor", "monitoring MAVLink packets")
+		self.status_callcount = 0
+		self.boredom_interval = 10 # seconds
+		self.last_bored = time.time()
+		self.jamming_on = False
+		self.cur_seqnum = None
 
-        #self.logfile = open('packet_log.txt', mode='w') # possible extension: receive filename as an argument
-        self.logger = logging.getLogger('logger')
+		#self.logfile = open('packet_log.txt', mode='w') # possible extension: receive filename as an argument
+		pathname = os.path.abspath('log/')
+		filename = datetime.now().strftime('%m%d_%H%M') + ".txt"
 
+		full_filename = os.path.join(pathname, filename)
 
-        self.packets_mytarget = 0
-        self.packets_othertarget = 0
-        self.verbose = False
-
-        self.example_settings = mp_settings.MPSettings(
-            [ ('verbose', bool, False),
-          ])
-        self.add_command('example', self.cmd_example, "example module", ['status','set (LOGSETTING)'])
-
-    def usage(self):
-        '''show help on command line options'''
-        return "Usage: example <status|set>"
-
-    def cmd_example(self, args):
-        '''control behaviour of the module'''
-        if len(args) == 0:
-            print self.usage()
-        elif args[0] == "status":
-            print self.status()
-        elif args[0] == "set":
-            self.example_settings.command(args[1:])
-        else:
-            print self.usage()
-
-    def status(self):
-        '''returns information about module'''
-        self.status_callcount += 1
-        self.last_bored = time.time() # status entertains us
-        return("status called %(status_callcount)d times.  My target positions=%(packets_mytarget)u  Other target positions=%(packets_mytarget)u" %
-               {"status_callcount": self.status_callcount,
-                "packets_mytarget": self.packets_mytarget,
-                "packets_othertarget": self.packets_othertarget,
-               })
-
-    def boredom_message(self):
-        if self.example_settings.verbose:
-            return ("I'm very bored")
-        return ("I'm bored")
-
-    def idle_task(self):
-        '''called rapidly by mavproxy'''
-        now = time.time()
-        if now-self.last_bored > self.boredom_interval:
-            self.last_bored = now
-            message = self.boredom_message()
-            self.say("%s: %s" % (self.name,message))
-            # See if whatever we're connected to would like to play:
-            self.master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_NOTICE,
-                                            message)
-
-    def mavlink_packet(self, m):
-        '''handle mavlink packets'''
+		self.logger = MAVPacketLogger(full_filename)
 
 
+		self.packets_mytarget = 0
+		self.packets_othertarget = 0
+		self.verbose = False
 
-        if m.get_type() == 'GLOBAL_POSITION_INT':
-            if self.settings.target_system == 0 or self.settings.target_system == m.get_srcSystem():
-                self.packets_mytarget += 1
-            else:
-                self.packets_othertarget += 1
+		self.example_settings = mp_settings.MPSettings(
+			[ ('verbose', bool, False),
+		  ])
+		self.add_command('example', self.cmd_example, "example module", ['status','set (LOGSETTING)'])
+		self.add_command('jamming', self.cmd_jamming, "jamming")
+
+	def usage(self):
+		'''show help on command line options'''
+		return "Usage: example <status|set>"
+
+	def cmd_jamming(self, args):
+
+		if len(args) == 0:
+			print self.usage()
+		elif args[0] == "start":
+			if self.jamming_on == False:
+				self.logger.log("JAMMING_START")
+				print 'Jamming start'
+				self.jamming_on = True
+			else:
+				print "Jamming signal is already on"
+		elif args[0] == "stop":
+			if self.jamming_on == True:
+				self.logger.log("JAMMING_STOP")
+				print 'Jamming strop'
+				self.jamming_on = False
+			else:
+				print "Jamming signal is already off"
+
+		
+		
+
+	def cmd_example(self, args):
+		'''control behaviour of the module'''
+		if len(args) == 0:
+			print self.usage()
+		elif args[0] == "status":
+			print self.status()
+			self.logger.log("JAMMING START")
+
+		elif args[0] == "set":
+			self.example_settings.command(args[1:])
+		elif args[0] == "test":
+			print "test!"
+		else:
+			print self.usage()
+
+	def status(self):
+		'''returns information about module'''
+		self.status_callcount += 1
+		self.last_bored = time.time() # status entertains us
+		return("status called %(status_callcount)d times.  My target positions=%(packets_mytarget)u  Other target positions=%(packets_mytarget)u" %
+			   {"status_callcount": self.status_callcount,
+				"packets_mytarget": self.packets_mytarget,
+				"packets_othertarget": self.packets_othertarget,
+			   })
+
+
+
+	def idle_task(self):
+		'''called rapidly by mavproxy'''
+		pass
+
+	def mavlink_packet(self, m):
+		'''handle mavlink packets'''
+		t = m.get_type()
+		
+
+		if t != 'RADIO' and t != 'RADIO_STATUS':
+			s = int(m.get_seq())
+
+			if self.cur_seqnum:
+				next_seqnum = self.cur_seqnum + 1
+				gap = (s - next_seqnum) % 256
+
+				if gap != 0:
+					dropped = range(next_seqnum, next_seqnum + gap)
+					dropped = [i % 256 for i in dropped]
+					print 'Jamming occured: %s got dropped' % str(dropped)
+
+			self.cur_seqnum = s
+
+			log_msg = "[%s] %s" % (s, t)
+			self.logger.log(log_msg)
+
+
+		
 
 def init(mpstate):
-    '''initialise module'''
-    return example(mpstate)
+	'''initialise module'''
+	return PacketMonitorModule(mpstate)
