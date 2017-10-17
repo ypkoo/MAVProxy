59a837e1a64d8258372b0048ca56aef772a52c43
ypkoo <ypkoo93@gmail.com>
2017-10-17 20:44:25 +0900

    log added

--- MAVProxy/log/result_parser.py
+++ MAVProxy/log/result_parser.py
@@ -4,10 +4,11 @@ if len(sys.argv) != 2:
 	sys.exit(0)
 
 filename = sys.argv[1]
+# index = sys.argv[2]
 
 f = open(filename, 'r')
 
 for line in f.readlines():
 	if len(line.split()) == 2:
-		print line.split(':')[0]
+		print line.split(':')[1].strip()
 
